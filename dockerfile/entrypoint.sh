0213346d743849943fde2535ef8cbdec926618c6
Yi Dong <yidong@nvidia.com>
2024-12-01 12:46:21 -0800

    enable sudo
    Signed-off-by: Yi Dong <yidong@nvidia.com>

--- dockerfile/entrypoint.sh
+++ dockerfile/entrypoint.sh
@@ -4,6 +4,11 @@
 if ! id -u $USER_ID >/dev/null 2>&1; then
     echo "myuser:x:$USER_ID:$GROUP_ID::/home/myuser:/bin/sh" >> /etc/passwd
     echo "myuser:x:$GROUP_ID:" >> /etc/group
+    mkdir -p /home/myuser
+    chown myuser:myuser -R /home/myuser
+    echo "myuser ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+    usermod -U myuser  # Unlock the user account
+     echo "myuser:password" | chpasswd  # Set a password for the user
 fi
 
 exec gosu myuser "$@"
